dd54292f4c8db599bef73f96c591cf6b3262b149
Loubet Gregory <Loubet Gregory@Gregory_Ordi>
2025-01-05 14:14:48 +0100

    Ajout des fichier Moteur
    Ajout de ses fichiers pour controler la vitesse et le sens des moteurs

---
+++ Robot_2_roue/Core/Fonction_Sup/Motorisation/Moteur.c
@@ -0,0 +1,9 @@
+/*
+ * Moteur.c
+ *
+ *  Created on: Jan 5, 2025
+ *      Author: Loubet Gregory
+ */
+
+#include "Moteur.h"
+
---
+++ Robot_2_roue/Core/Fonction_Sup/Motorisation/Moteur.h
@@ -0,0 +1,14 @@
+/*
+ * Moteur.h
+ *
+ *  Created on: Jan 5, 2025
+ *      Author: Loubet Gregory
+ */
+
+#ifndef FONCTION_SUP_MOTORISATION_MOTEUR_H_
+#define FONCTION_SUP_MOTORISATION_MOTEUR_H_
+
+class Moteur {
+};
+
+#endif /* FONCTION_SUP_MOTORISATION_MOTEUR_H_ */
